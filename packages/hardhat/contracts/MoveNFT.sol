ffedf5e9a8dc03cf6e4a23f5790bacaaec88106b
codechefsong <133816663+codechefsong@users.noreply.github.com>
2023-09-16 17:27:48 -0400

    add moves feature

--- packages/hardhat/contracts/MoveNFT.sol
+++ packages/hardhat/contracts/MoveNFT.sol
@@ -8,14 +8,29 @@ contract MoveNFT is ERC721URIStorage {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
+  Move[] public moves;
+  mapping(address => Move[]) public playerMoves;
+
   constructor() ERC721("Move NFT", "MOVE") {}
 
-  function mint(address _to, string memory _tokenURI_) public returns (uint256) {
+  struct Move {
+    uint256 id;
+    string name;
+    uint256 power;
+  }
+
+  function getMyMoves(address _owner) public view returns (Move[] memory){
+    return playerMoves[_owner];
+  }
+
+  function mint(address _to, string memory _tokenURI_, string calldata _name, uint256 _power) public returns (uint256) {
     uint256 newItemId = _tokenIds.current();
     _mint(_to, newItemId);
     _setTokenURI(newItemId, _tokenURI_);
 
     _tokenIds.increment();
+    moves.push(Move(newItemId, _name, _power));
+    playerMoves[msg.sender].push(Move(newItemId, _name, _power));
     return newItemId;
   }
 }
